cb38467b14b6ce98ef27f22e15573f335614801d
root <ester.ovrani@gmail.com>
2025-09-14 16:01:44 +0300

    Add Redis dependencies

---
+++ src/main/java/com/smartdocumentchat/RedisConfig.java
@@ -0,0 +1,68 @@
+package com.smartdocumentchat;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+public class RedisConfig {
+
+    @Value("${redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${redis.port:6379}")
+    private int redisPort;
+
+    @Value("${redis.password:smartdoc_redis_pass}")
+    private String redisPassword;
+
+    @Value("${redis.database:0}")
+    private int redisDatabase;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        log.info("Creating Redis connection factory - Host: {}, Port: {}, Database: {}",
+                redisHost, redisPort, redisDatabase);
+
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(redisHost);
+        redisConfig.setPort(redisPort);
+        redisConfig.setPassword(redisPassword);
+        redisConfig.setDatabase(redisDatabase);
+
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisConfig);
+
+        log.info("Redis connection factory created successfully");
+        return factory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        log.info("Creating Redis template");
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // Use String serializer for keys
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        // Use JSON serializer for values
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        template.setEnableTransactionSupport(true);
+        template.afterPropertiesSet();
+
+        log.info("Redis template configured successfully");
+        return template;
+    }
+}
